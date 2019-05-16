50ea726740f664a323672d76ba2f7dca43a08348
omkar-hardwell <omkarkshirsagar862@gmail.com>
2019-05-16 17:26:23 +0530

    Added the Service and Repository layer

--- app/Employee.php
+++ app/Employee.php
@@ -8,5 +8,15 @@ class Employee extends Model
 {
     // Employee model
 
+    /**
+     * Table name.
+     * @var string
+     */
+    protected $table = 'employees';
+
+    /**
+     * Columns used in operation.
+     * @var array
+     */
     protected $fillable = ['employee_id', 'department_id', 'name', 'date_of_joining', 'gender', 'address', 'salary'];
 }
---
+++ app/Http/Repository/CRUDDemoRepositoryEmployee.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Repository;
+
+use App\Employee;
+
+class CRUDDemoRepositoryEmployee extends Repository
+{
+    // CRUDDemoRepositoryEmployee repository class.
+
+    /**
+     * @var Employee|null
+     */
+    protected $_employee = null;
+
+    /**
+     * CRUDDemoRepositoryEmployee constructor.
+     * @param Employee $_employee
+     */
+    public function __construct(Employee $_employee)
+    {
+        $this->_employee = $_employee;
+    }
+
+    /**
+     * Fetch employee.
+     * @param null $employeeId
+     * @return mixed
+     */
+    public function getEmployee($employeeId = null)
+    {
+        return ($employeeId) ?
+            Employee::where('employee_id', $employeeId)->first() :
+            Employee::join('departments', 'employees.department_id', '=', 'departments.department_id')
+            ->select('employees.employee_id', 'employees.name As emp_name', 'employees.date_of_joining',
+                'employees.gender', 'employees.address', 'employees.salary', 'departments.name')
+            ->get()->toArray();
+    }
+
+    /**
+     * Add employee.
+     * @param $employeeDetail
+     */
+    public function addEmployee($employeeDetail)
+    {
+        $employee = new Employee($employeeDetail);
+        $employee->save();
+    }
+
+    /**
+     * Update employee.
+     * @param $employeeId
+     * @param $employeeDetail
+     */
+    public function updateEmployee($employeeId, $employeeDetail)
+    {
+        Employee::where('employee_id', '=', $employeeId)
+            ->update($employeeDetail);
+    }
+
+    /**
+     * Delete employee.
+     * @param $employeeId
+     */
+    public function deleteEmployee($employeeId)
+    {
+        Employee::where('employee_id', '=', $employeeId)->delete();
+    }
+}
